6c45a7ec597e1d53688757d3cea4a5d8b3a8dcd1
Khai Dang <akhai7230@gmail.com>
2025-05-27 23:06:14 +0700

    Update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const frontendBuildPath = path.join(__dirname, 'dist');
 app.use(express.static(frontendBuildPath));
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:5173', 'https://ai.miseninc.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
