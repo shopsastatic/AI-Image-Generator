189e83f65f5070edae08e90818681da9b8454bd0
Khai Dang <akhai7230@gmail.com>
2025-10-30 18:38:49 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const isProduction = mode === 'production';
+  const isProduction = mode === 'production1';
   
   const backendUrl = env.VITE_API_BASE_URL || 'http://localhost:3001';
   
